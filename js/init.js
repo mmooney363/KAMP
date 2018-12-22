1d1b50188805f0ad4967d70a35abfa1e1815b687
afeldmanx1 <alexf0922@gmail.com>
2018-12-22 11:55:39 -0500

    scroll bar added

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    $(document).ready(function(){
+      $('select').formSelect();
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
