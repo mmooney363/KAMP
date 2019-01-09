885f3ba38016c1250a911bee587fb06437528f69
Mike Mooney <mmooney363@gmail.com>
2019-01-08 19:48:13 -0500

    weather update

---
+++ js/weather.js
@@ -0,0 +1,12 @@
+var lat = "NPSlat"
+var lon = "NPSlon";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=7d303e69b0351c31f4dd317a06e61fed";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+  console.log(response.Runtime);
+});
+
