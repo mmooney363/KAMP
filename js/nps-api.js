b106bcb9b3fecc200a6cc082e7ebbde9e6a159a2
Spatula Pete <petewx26@gmail.com>
2019-01-09 18:03:27 -0500

    modify nps-api.js to properly pull from dropdown list. prints to div and clears with new search

--- js/nps-api.js
+++ js/nps-api.js
@@ -1,9 +1,9 @@
 var apiKey = "kjVPCM3LEwQYZxkv4byFPo1TRCvJk61IAO9wwtyI";
 var searchTerm = "";
-var stateTerm = "CA";
+var stateTerm = "";
+var queryURL = "";
 
-var queryURL = "https://api.nps.gov/api/v1/parks?q=" + searchTerm + "&stateCode=" + stateTerm + "&api_key=" + apiKey;
-console.log(queryURL);
+
 
 
 var parkName = "";
@@ -11,13 +11,35 @@ var parkDescription = "";
 var parkLatLon = "";
 var parkURL = "";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelector('select');
+    elems.onchange = selectThem;
+    var instances = M.FormSelect.init(elems);
+    function selectThem() {
+        var selectedOne = instances.getSelectedValues();
+        console.log(selectedOne);
+        stateTerm = selectedOne.join();
+        console.log(stateTerm);
+        queryURL = "https://api.nps.gov/api/v1/parks?q=" + searchTerm + "&stateCode=" + stateTerm + "&api_key=" + apiKey;
+        console.log(queryURL);
+    }
 
-        for (var i = 0; i < response.data.length; i++) {
-          parkName = response.data[i].fullName; 
-          $("#search-results").append("<li>" + parkName + "</li>");
-        }
-    });
+    $("#search-btn").click(function() {
+
+        $("#search-results").empty();
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+        
+            for (var i = 0; i < response.data.length; i++) {
+              parkName = response.data[i].fullName; 
+              $("#search-results").append("<li>" + parkName + "</li>");
+            }
+        });
+    })
+});
+
+
+
