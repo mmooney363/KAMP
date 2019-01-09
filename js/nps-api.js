a64a5357c1417bb6c6255fd9b872a3b5ca5758a3
Spatula Pete <petewx26@gmail.com>
2019-01-08 19:40:15 -0500

    add js/nps-api.js modify index.html to link to nps-api.js

---
+++ js/nps-api.js
@@ -0,0 +1,23 @@
+var apiKey = "kjVPCM3LEwQYZxkv4byFPo1TRCvJk61IAO9wwtyI";
+var searchTerm = "";
+var stateTerm = "CA";
+
+var queryURL = "https://api.nps.gov/api/v1/parks?q=" + searchTerm + "&stateCode=" + stateTerm + "&api_key=" + apiKey;
+console.log(queryURL);
+
+
+var parkName = "";
+var parkDescription = "";
+var parkLatLon = "";
+var parkURL = "";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        for (var i = 0; i < response.data.length; i++) {
+          parkName = response.data[i].fullName; 
+          $("#test-results").append("<li>" + parkName + "</li>");
+        }
+    });
