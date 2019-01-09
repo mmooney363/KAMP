140e485c126194e201b159e5dba82bf67c6094de
afeldmanx1 <alexf0922@gmail.com>
2019-01-08 19:12:55 -0500

    full design update

--- js/init.js
+++ js/init.js
@@ -1,10 +1,12 @@
 (function($){
   $(function(){
-
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $(document).ready(function(){
       $('select').formSelect();
+      
     });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
