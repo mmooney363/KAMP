9c915c452db44731e73ec2de6d054e21ab346368
afeldmanx1 <alexf0922@gmail.com>
2019-01-10 19:06:07 -0500

    sidebar fix

--- js/init.js
+++ js/init.js
@@ -1,12 +1,12 @@
 (function($){
   $(function(){
-    $('.sidenav').sidenav();
+    
     $('.parallax').parallax();
     $(document).ready(function(){
+      $('.sidenav').sidenav();
       $('select').formSelect();
+      
       
     });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-
